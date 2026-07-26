11528d0ce2c0b60d9b90497fce21f2b379c7d3d6
Shrimant <145121082+Shri32@users.noreply.github.com>
2026-07-26 20:31:18 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        
+        int r= *max_element(piles.begin(), piles.end());
+        int ans= r;
+        while(l <= r){
+
+            int mid = l + (r-l)/2;
+            int caneat=0;
+        for(int i=0;i<piles.size();i++){
+        caneat += ceil((double)piles[i]/mid);
+        }
+        if(caneat <= h){
+            ans= mid;
+            r= mid -1;
+        }
+        else l= mid+1;
+        }
+        return ans;
+    }
+};
